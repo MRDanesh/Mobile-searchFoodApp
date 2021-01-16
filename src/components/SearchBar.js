8fad6e1279e178e0e8f052b7dd603327d5f0142d
MRDanesh <mohammadrezad126@gmail.com>
2021-01-16 16:09:04 -0500

    add yelp search with hooks

---
+++ src/components/SearchBar.js
@@ -0,0 +1,42 @@
+import React from 'react';
+import {Text, StyleSheet, View} from 'react-native';
+import {Feather} from '@expo/vector-icons';
+import { TextInput } from 'react-native-gesture-handler';
+
+const SearchBar = ({term, onTermChange, onTermSubmit}) => {
+    return (
+        <View style={styles.backgroundStyle}>
+            <Feather style={styles.iconStyle} name= 'search' />
+            <TextInput 
+            value={term} 
+            onChangeText={(newTerm) => onTermChange(newTerm)} 
+            onEndEditing={()=>onTermSubmit()}
+            style={styles.inputStyle} placeholder="search" 
+            />
+        </View>
+    );
+};
+
+const styles = StyleSheet.create({
+    backgroundStyle: {
+        marginTop: 10,
+        backgroundColor: '#F0EEEE',
+        height: 50,
+        borderRadius: 5,
+        marginHorizontal: 15,
+        flexDirection: 'row'
+    },
+    inputStyle: {
+        flex: 1,
+        fontSize: 18
+    },
+    iconStyle: {
+        fontSize: 35,
+        alignSelf: 'center',
+        marginHorizontal: 15
+    }
+
+
+});
+
+export default SearchBar;
--- src/screens/SearchScreen.js
+++ src/screens/SearchScreen.js
@@ -1,14 +1,42 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {Text, StyleSheet, View} from 'react-native';
 
+import SearchBar from '../components/SearchBar';
+import yelp from '../../src/api/yelp';
+
 const SearchScreen = () => {
+
+    const [term, setTerm] = useState ('');
+    const [results, setResults] = useState([]);
+
+    const searchApi = async () => {
+        const response = await yelp.get('/search', {
+            params:{
+                limit: 50,
+                term,
+                location: 'Montreal'
+            }
+        });
+        setResults(response.data.businesses);
+    };
+    
+
     return (
-        <View>
-            <Text>Search Screen</Text>
+        <View style={styles.background}>
+            <SearchBar 
+            term={term} 
+            onTermChange = {(newTerm) => setTerm(newTerm)}
+            onTermSubmit = {() => searchApi()}
+            />
+            <Text>We found {results.length} results</Text>
         </View>
     );
 };
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+    background: {
+        backgroundColor: 'white'
+    }
+});
 
 export default SearchScreen;
