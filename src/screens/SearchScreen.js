3d0bb64365bf44670b7aa3956db3ee49dcc42c56
MRDanesh <mohammadrezad126@gmail.com>
2021-02-11 22:45:39 -0500

    transfer

--- src/screens/SearchScreen.js
+++ src/screens/SearchScreen.js
@@ -3,21 +3,30 @@ import {Text, StyleSheet, View} from 'react-native';
 
 import SearchBar from '../components/SearchBar';
 import useResults from '../../src/hooks/useResults';
+import ResultsList from '../../src/components/ResultsList';
 
 const SearchScreen = () => {
 
     const [term, setTerm] = useState ('');
     const [results, errorMessage, searchApi] = useResults();
     
+    const filterResultsByPrice = (price) => {
+        return results.filter(result => {
+            return result.price === price;
+        });
+    };
     
 
     return (
         <View style={styles.background}>
             <SearchBar 
-            term={term} 
-            onTermChange = {(newTerm) => setTerm(newTerm)}
-            onTermSubmit = {() => searchApi(term)}
+                term={term} 
+                onTermChange = {(newTerm) => setTerm(newTerm)}
+                onTermSubmit = {() => searchApi(term)}
             />
+            <ResultsList results={filterResultsByPrice('$')} title='Cost Effective'/>
+            <ResultsList results={filterResultsByPrice('$$')} title='Pricier'/>
+            <ResultsList results={filterResultsByPrice('$$$')} title='Expensive'/>
             {errorMessage ? <Text>{errorMessage}</Text>:null}
             <Text>We found {results.length} results</Text>
         </View>
