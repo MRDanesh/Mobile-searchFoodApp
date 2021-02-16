eead0c85c8ef3587a1c1f8a38cc033cac7258991
Mohammad Reza Daneshvar Garmroodi <mohammadreza@Mohammads-MacBook-Pro.local>
2021-02-16 11:02:05 -0500

    finished

--- src/components/ResultsDetail.js
+++ src/components/ResultsDetail.js
@@ -2,13 +2,26 @@ import React from 'react';
 import {View, Image, Text, StyleSheet} from 'react-native';
 
 const ResultsDetail = ({result}) => {
+    console.log(result);
     return(
-        <View>
-            <Text>{result.name}</Text>
+        <View style={{marginLeft:15}}>
+            <Image style={styles.image} source={{uri: result.image_url}} />
+            <Text style={styles.name} >{result.name}</Text>
+            <Text> {result.rating} stars {result.review_count} reviews </Text>
         </View>
     );
 };
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+    image: {
+        width: 250,
+        height: 120,
+        borderRadius: 4,
+        marginBottom: 10
+    },
+    name: {
+        fontWeight: 'bold'
+    }
+});
 
 export default ResultsDetail;
--- src/screens/SearchScreen.js
+++ src/screens/SearchScreen.js
@@ -1,5 +1,5 @@
 import React, {useState, useEffect} from 'react';
-import {Text, StyleSheet, View} from 'react-native';
+import {Text, StyleSheet, ScrollView, View} from 'react-native';
 
 import SearchBar from '../components/SearchBar';
 import useResults from '../../src/hooks/useResults';
@@ -24,18 +24,30 @@ const SearchScreen = () => {
                 onTermChange = {(newTerm) => setTerm(newTerm)}
                 onTermSubmit = {() => searchApi(term)}
             />
-            <ResultsList results={filterResultsByPrice('$')} title='Cost Effective'/>
-            <ResultsList results={filterResultsByPrice('$$')} title='Pricier'/>
-            <ResultsList results={filterResultsByPrice('$$$')} title='Expensive'/>
-            {errorMessage ? <Text>{errorMessage}</Text>:null}
-            <Text>We found {results.length} results</Text>
+            <ScrollView>
+                <ResultsList
+                 results={filterResultsByPrice('$')} 
+                 title='Cost Effective'
+                 />
+                <ResultsList 
+                    results={filterResultsByPrice('$$')} 
+                    title='Pricier'
+                />
+                <ResultsList 
+                    results={filterResultsByPrice('$$$')} 
+                    title='Expensive'
+                />
+                {errorMessage ? <Text>{errorMessage}</Text>:null}
+            </ScrollView>
+            
         </View>
     );
 };
 
 const styles = StyleSheet.create({
     background: {
-        backgroundColor: 'white'
+        backgroundColor: 'white',
+        flex: 1
     }
 });
 
